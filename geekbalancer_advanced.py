8b0503f30a91dd2922cce03b9bc605478b568ff9
Edge <edge@edgemods.com>
2023-09-16 11:28:41 -0400

    Returns discord and steam id for each player

--- geekbalancer_advanced.py
+++ geekbalancer_advanced.py
@@ -103,10 +103,8 @@ def filter_player_stats(data, players):
     mydata_parsed = json.loads(data)
     filtered_data = []
     for player in players:
-        print(f"Filtering stats for player '{player}'...")
         for entry in mydata_parsed:
             if entry['player'] == player:
-                print(f"Adding player: {entry['player']}")
                 filtered_data.append(entry)
                 break
 
@@ -212,10 +210,6 @@ def assign_players(data, threshold):
     # Sort players by composite score
     scores.sort(key=lambda x: x[1], reverse=True)
     
-    print("\nPLAYER\n")
-    print(player)
-    print("")
-
     # Assign players to teams
     team_a = []
     team_b = []
@@ -294,22 +288,21 @@ def print_top_teams(teams, max_teams=10):
             print(f" - {name} ({score:.4f})")
         print()
 
-def create_team_json_list(team, team_name, team_score):
+def create_team_json_list(team, team_name, team_score, player_dict):
     players = []
     for name, score in team:
-        # Get discord_id from players
-        discord_id = next((player['discord_id'] for player in players if player['player_name'] == name), None)
-        # Get steam_id from players
-        steam_id = next((player['steam_id'] for player in players if player['player_name'] == name), None)
-        # Get captain from players
-        captain = next((player['captain'] for player in players if player['player_name'] == name), None)
-        print(f"Discord ID: {discord_id}")
-        print(f"Steam ID: {steam_id}")
-        print(f"Captain: {captain}")
+
+        # get discord name for the specified player handle from player_dict
+        discord = player_dict[name]['discord']
+
+        # get steam_id for the specified player handle from player_dict
+        steam_id = player_dict[name]['steam_id']
 
         player = {
             'player_name': name,
-            'player_score': round(score, 4)
+            'player_score': round(score, 4),
+            'discord': discord,
+            'steam_id': steam_id
         }
         players.append(player)
     team_json = {
@@ -320,18 +313,18 @@ def create_team_json_list(team, team_name, team_score):
     }
     return team_json
 
-def get_top_teams(teams, max_teams=10, output_file='top_teams.json'):
+def get_top_teams(teams, player_dict, max_teams=10, output_file='top_teams.json'):
     sorted_teams = sorted(teams, key=lambda x: abs(sum([score for _, score in x[0]]) - sum([score for _, score in x[1]])))
     top_teams = []
     for team_a, team_b in sorted_teams[:max_teams]:
         team_a_score = sum([score for _, score in team_a])
         team_b_score = sum([score for _, score in team_b])
-        team_a_json = create_team_json_list(team_a, 'Alpha', team_a_score)
-        team_b_json = create_team_json_list(team_b, 'Bravo', team_b_score)
+        team_a_json = create_team_json_list(team_a, 'Alpha', team_a_score, player_dict)
+        team_b_json = create_team_json_list(team_b, 'Bravo', team_b_score, player_dict)
         top_teams.append({'team_a': team_a_json, 'team_b': team_b_json})
     with open(output_file, 'w') as f:
         json.dump(top_teams, f, indent=4)
-    print(json.dumps(top_teams, indent=4))
+    #print(json.dumps(top_teams, indent=4))
     return(top_teams)
 
 def filter_teams_by_captains(data, captain1, captain2):
@@ -385,19 +378,20 @@ def balance_teams_api():
     api_string  = create_api_string(base_url, start_date, end_date)
     statsURL    = api_string
 
-    # Get the latest stats for all players
+    # Get the latest stats for all players (passed on the start and end date)
+    # We get all the stats for all the players on the server - then we filter based on who is currently in discord
     all_stats = get_json_from_api(statsURL)
     print("STATS FROM API [all_stats]]")
     print(all_stats)
     print("")
 
-    # Get the list of players from the JSON payload
+    # Get the list of players being passed to the API from discord via JSON
     discord_players = request.get_json()
     print("PLAYERS FROM DISCORD [discord_players]")
     print(discord_players)
     print("")
 
-    # Get the captains from discord_players
+    # Get the captains from discord_players (should be 2)
     captains = []
     for player in discord_players:
         if player['captain'] == 'TRUE':
@@ -406,22 +400,23 @@ def balance_teams_api():
     print(captains)
     print("")
 
-    # Create an empty dictionary to store the player data
+    # Create an empty dictionary to store the player data (discord name, steam id, csgo handle)
+    # This makes it easier to pass this information to the final function that creates the teams
+    # The discord bot needs this info
     player_dict = {}
-
     # Loop through each player in the discord players list
     for player in discord_players:
         # Add the player data to the dictionary using the player name as the key
         player_dict[player['handle']] = {
             'steam_id': player['steam_id'],
-            'handle': player['handle']
+            'discord': player['discord']
         }
     print("PLAYER DICTIONARY [player_dict]")
     print(player_dict)
     print("")
-    print(get_player_data('Edge', player_dict))
 
     # Get the list of player names from discord_players
+    # We use this simple list to filter the stats to only the current players in discord
     print("PLAYER HANDLES FROM DISCORD [player_handles]")
     player_handles = []
     for player in discord_players:
@@ -429,13 +424,13 @@ def balance_teams_api():
     print(player_handles)
     print("")
     
-    # Filter stats for only the specified player handles from the discord json payload
+    # Filter stats for only the specified player handles
     filtered_stats = filter_player_stats(all_stats, player_handles)
     print("FILTERED STATS [filtered_stats]")
     print(filtered_stats)
     print("")
 
-    # Create balanced teams within the specified threshold
+    # Create balanced teams within the specified threshold for only the filtered players
     teams = balance_teams(filtered_stats, threshold)
 
     # Check if balanced teams were possible for the specified players
@@ -443,17 +438,14 @@ def balance_teams_api():
         return jsonify({'error': 'Cannot balance teams with the given threshold and maximum number of attempts.'}), 400
 
     # Return the specified number of teams (sorted by difference in team scores in descending order)
-    top_teams = get_top_teams(teams, 10)
-
+    top_teams = get_top_teams(teams, player_dict, 10)
 
     # Serialize the response data to JSON format
     # Send back the top_teams
     response_data = json.dumps(top_teams)
-    print("RESPONSE DATA")
-    print(response_data)
-    print("")
 
-
+    # filter the response data to only include the teams with the specified captains
+    
     #print("FILTERED TEAMS")
     #print("")
     #print(captains[0])
@@ -461,9 +453,6 @@ def balance_teams_api():
     #print("")
     #filtered_data = filter_teams_by_captains(response_data, captains[0], captains[1])
     #print(json.dumps(filtered_data, indent=4))
-
-    # Serialize the response data to JSON format
-    #response_data = json.dumps(filtered_data)
 
     # Set the Content-Type header to indicate that the response is in JSON format
     headers = {'Content-Type': 'application/json'}
